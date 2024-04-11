94b4ceac3140adbf1be11a16903924ee1eedadc2
vanger2607 <vanger2607@gmail.com>
2024-04-11 21:09:40 +0300

    add updateRating func

--- appModules/ratings/calculations.js
+++ appModules/ratings/calculations.js
@@ -1,7 +1,12 @@
 function createRating(obj, weight) {
-    return (
-      obj.gameplay * weight.gameplay +
-      obj.design * weight.design +
-      obj.idea * weight.idea
-    );
-  } 
+  return (
+    obj.gameplay * weight.gameplay +
+    obj.design * weight.design +
+    obj.idea * weight.idea
+  );
+}
+function updateRating(array, id, rating) {
+  const index = array.findIndex((item) => item.id === id);
+  array[index].rating += rating;
+  return array;
+}
