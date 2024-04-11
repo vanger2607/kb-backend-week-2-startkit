868991825641856cd11dca7096963a89e5f67a05
vanger2607 <vanger2607@gmail.com>
2024-04-11 21:02:03 +0300

    update gameRouterContoroller

--- controllers/game.js
+++ controllers/game.js
@@ -1,18 +1,19 @@
 const fs = require("fs").promises;
 const { config } = require("../appModules/ratings");
 
+const { getRandomGame } = require("../appModules/api");
+
 async function gameRouteController(res) {
   try {
     const ratingFile = await fs.readFile(config.PATH_TO_RATING_FILE);
     const data = JSON.parse(ratingFile);
-    const game = data[0];
+    const game = getRandomGame(data); // Получаем случайную игру
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(game));
-  } catch (err) {
+  } catch (error) {
     res.statusCode = 500;
-    console.log(config.PATH_TO_RATING_FILE);
     res.end("Internal Server Error");
   }
-}
+} 
 
 module.exports = gameRouteController;
